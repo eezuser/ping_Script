e8559ad48b338cab9a3afc8a052d598ac4e13c53
waleola965 <waleola965@yahoo.com>
2023-03-10 18:54:11 -0500

    Ask user input

--- ping.py
+++ ping.py
@@ -3,6 +3,9 @@
 from pythonping import ping
 import ipaddress
 
+#Ask for user network/input
+#input(str("Enter ip and prefix:"))
+
 network = ipaddress.ip_network('127.0.0.0/24')
 
 for addr in network.hosts():
